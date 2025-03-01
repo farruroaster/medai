c4a9b9e30035b872c52dcd68b80ae7943a1cf423
Farhan <farhan@mac.mynetworksettings.com>
2025-03-01 12:50:39 -0500

    Initial commit - Flask setup

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "MedAI API is running!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
